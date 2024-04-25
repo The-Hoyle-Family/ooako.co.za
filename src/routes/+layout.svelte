bd176b9c85e1a5f562a0883021ebea28041e9821
Tagan H <tagnum.elite@gmail.com>
2024-04-25 21:45:36 +0200

    Run prettier, pretty sure it's uglier now
    Also some quotes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,109 +2,10 @@
 	export let data;
 </script>
 
-<style lang="scss">
-abbr[title] {
-	border-bottom: none !important;
-  	cursor: inherit !important;
-  	text-decoration: none !important;
-}
-
-header {
-	position: fixed;
-	top: 0;
-	left: 0;
-	right: 0;
-	text-align: center;
-	color: #FFF;
-	background-color: #000;
-
-	a {
-		color: #FFF;
-		padding: 14px 16px;
-		text-decoration: none;
-
-		&:hover {
-			color: #F0F;
-		}
-	}
-
-	#logo {
-		float: left;
-		padding: 12px 16px;
-		font-size: 2.5rem;
-		margin-left: 3rem;
-	}
-
-	.navbar {
-		float: right;
-		height: 100%;
-
-		ul {
-			list-style: none;
-			padding: 0;
-		}
-
-		li {
-			display: inline;
-			margin-right: 10px;
-		}
-	}
-}
-
-main {
-	margin-top: 3rem;
-	padding-top: 1rem;
-	min-height: 70vh;
-}
-
-footer {
-	text-align: center;
-	width: 100%;
-	border-top: 6px solid blue;
-	color: #FFF;
-	background-color: #000;
-	padding-bottom: 3rem;
-	padding-top: 3rem;
-
-	.socials {
-		display: inline-flex;
-
-		.social {
-			padding: 12px;
-
-			img {
-				filter: invert(99%) sepia(6%) saturate(571%) hue-rotate(222deg) brightness(119%) contrast(100%);
-			}
-
-			a:hover img {
-				filter: invert(65%) sepia(68%) saturate(3793%) hue-rotate(136deg) brightness(107%) contrast(101%);
-			}
-		}
-	}
-
-	.links {
-
-	}
-
-	.dev-info {
-		color: #adadad;
-
-		a {
-			text-decoration: none;
-			color: #444444;
-
-			&:hover {
-				color: #b1b1b1;
-			}
-		}
-	}
-}
-</style>
-
 <header>
-    <div id="logo">
-        <abbr title="One Of A Kind Original">OOAKO</abbr>
-    </div>
+	<div id="logo">
+		<abbr title="One Of A Kind Original">OOAKO</abbr>
+	</div>
 	<nav class="navbar">
 		<ul>
 			{#each data.nav as nav}
@@ -119,13 +20,124 @@ footer {
 <footer>
 	<div id="socials" class="socials">
 		{#each data.socials as social}
-		<div class="social {social.slug}"><a href={social.href}><img alt={social.name} height="32" width="32" src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/{social.slug}.svg" /></a></div>
+			<div class="social {social.slug}">
+				<a href={social.href}
+					><img
+						alt={social.name}
+						height="32"
+						width="32"
+						src="https://cdn.jsdelivr.net/npm/simple-icons@v11/icons/{social.slug}.svg"
+					/></a
+				>
+			</div>
 		{/each}
-	</div>	
-	<div class="links" id="footer-links">
-
 	</div>
+	<div class="links" id="footer-links"></div>
 	<div class="dev-info">
-		<p>Created/Maintained by <a href="https://tagnumelite.com">Tagan Hoyle</a>. Powered by <a href="https://kit.svelte.dev/">SvelteKit</a> using <a href="https://pages.cloudflare.com/">Cloudfare Pages</a>.</p>
+		<p>
+			Created/Maintained by <a href="https://tagnumelite.com">Tagan Hoyle</a>. Powered by
+			<a href="https://kit.svelte.dev/">SvelteKit</a>
+			using <a href="https://pages.cloudflare.com/">Cloudfare Pages</a>.
+		</p>
 	</div>
 </footer>
+
+<style lang="scss">
+	abbr[title] {
+		border-bottom: none !important;
+		cursor: inherit !important;
+		text-decoration: none !important;
+	}
+
+	header {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		text-align: center;
+		color: #fff;
+		background-color: #000;
+
+		a {
+			color: #fff;
+			padding: 14px 16px;
+			text-decoration: none;
+
+			&:hover {
+				color: #f0f;
+			}
+		}
+
+		#logo {
+			float: left;
+			padding: 12px 16px;
+			font-size: 2.5rem;
+			margin-left: 3rem;
+		}
+
+		.navbar {
+			float: right;
+			height: 100%;
+
+			ul {
+				list-style: none;
+				padding: 0;
+			}
+
+			li {
+				display: inline;
+				margin-right: 10px;
+			}
+		}
+	}
+
+	main {
+		margin-top: 3rem;
+		padding-top: 1rem;
+		min-height: 70vh;
+	}
+
+	footer {
+		text-align: center;
+		width: 100%;
+		border-top: 6px solid blue;
+		color: #fff;
+		background-color: #000;
+		padding-bottom: 3rem;
+		padding-top: 3rem;
+
+		.socials {
+			display: inline-flex;
+
+			.social {
+				padding: 12px;
+
+				img {
+					filter: invert(99%) sepia(6%) saturate(571%) hue-rotate(222deg) brightness(119%)
+						contrast(100%);
+				}
+
+				a:hover img {
+					filter: invert(65%) sepia(68%) saturate(3793%) hue-rotate(136deg) brightness(107%)
+						contrast(101%);
+				}
+			}
+		}
+
+		.links {
+		}
+
+		.dev-info {
+			color: #adadad;
+
+			a {
+				text-decoration: none;
+				color: #444444;
+
+				&:hover {
+					color: #b1b1b1;
+				}
+			}
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,81 +1,96 @@
 <script>
-    import Quote from "$lib/Quote.svelte";
+	import Quote from '$lib/Quote.svelte';
 </script>
 
-<style lang="scss">
-.jumbotron {
-    width: 100%;
-    background: url("/main-jumbo.png") no-repeat top ;
-    background-size: 100vw;
-    text-align: center;
-    color: #e4e4e4;
-    text-shadow: 2px 2px #000;
-    border-bottom: 2px solid #000;
-
-    .container {
-        padding: 7rem 3rem;
-    }
-}
-
-.container {
-        margin-left: auto;
-        margin-right: auto;
-        width: 100%;
-
-    .title {
-        font-size: 4rem;
-
-        &:first-child p {
-            margin-bottom: 0;
-        }
-
-        .sub {
-            font-size: 1.5rem;
-            margin-top: 0;
-        }
-    }
-
-    .info p {
-        text-shadow: 1px 1px #000;
-    }
-
-    &.inline {
-        display: inline-flex;
-        flex-wrap: wrap;
-    }
-}
-</style>
-
 <div class="jumbotron">
-    <div class="container">
-        <div class="title">
-            <p>Linda Ann Hoyle</p>
-            <p class="sub">1952-2024 (72 years)</p>
-        </div>
-        <div class="info">
-            <p>A mother, artist and light of the community</p>
-            <p>Someone who was loved by all and hated by none</p>
-            <p>Her creativity knew no bounds, art was her life</p>
-            <p>Her smile lit up any room, made any place feel like home</p>
-            <p></p>
-        </div>
-    </div>
+	<div class="container">
+		<div class="title">
+			<p>Linda Ann Hoyle</p>
+			<p class="sub">1952-2024 (72 years)</p>
+		</div>
+		<div class="info">
+			<p>A mother, artist and light of the community</p>
+			<p>Someone who was loved by all and hated by none</p>
+			<p>Her creativity knew no bounds, art was her life</p>
+			<p>Her smile lit up any room, made any place feel like home</p>
+			<p></p>
+		</div>
+	</div>
 </div>
 <div class="container inline">
-    <Quote author="Andrew H">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis massa quis augue luctus, nec iaculis orci maximus. Pellentesque finibus. 
-    </Quote>
-    <Quote author="Tagan H">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis massa quis augue luctus, nec iaculis orci maximus. Pellentesque finibus. 
-    </Quote>
-    <Quote author="Lee H">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis massa quis augue luctus, nec iaculis orci maximus. Pellentesque finibus. 
-    </Quote>
-    <Quote author="Zara DB">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis massa quis augue luctus, nec iaculis orci maximus. Pellentesque finibus. 
-    </Quote>
-    <Quote author="Jacques DB">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis massa quis augue luctus, nec iaculis orci maximus. Pellentesque finibus. 
-    </Quote>
+	<div>
+		<Quote author="Andrew H">
+			My wife for over 50 years and my best friend through it all. She always complained about the
+			mess I left but otherwise, she has always been friendly and happy.
+		</Quote>
+	</div>
+	<div>
+		<Quote author="Tagan H">
+			I never treated granny the way a grandson should've but she still took care of me, clothed me
+			and fed me. I was never without shelter, food and clothes. She made sure everyone was taken
+			care of.
+		</Quote>
+	</div>
+	<div><Quote author="Lee H">What, mom is dead? Oh, ok. Can you take me to the pub?</Quote></div>
+	<div>
+		<Quote author="Zara DB">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis massa quis augue luctus,
+			nec iaculis orci maximus. Pellentesque finibus.
+		</Quote>
+	</div>
+	<div>
+		<Quote author="Jacques DB">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis massa quis augue luctus,
+			nec iaculis orci maximus. Pellentesque finibus.
+		</Quote>
+	</div>
 </div>
 
+<style lang="scss">
+	.jumbotron {
+		width: 100%;
+		background: url('/main-jumbo.png') no-repeat top;
+		background-size: 100vw;
+		text-align: center;
+		color: #e4e4e4;
+		text-shadow: 2px 2px #000;
+		border-bottom: 2px solid #000;
+
+		.container {
+			padding: 7rem 3rem;
+		}
+	}
+
+	.container {
+		margin-left: auto;
+		margin-right: auto;
+        padding: 5px;
+
+		.title {
+			font-size: 4rem;
+
+			&:first-child p {
+				margin-bottom: 0;
+			}
+
+			.sub {
+				font-size: 1.5rem;
+				margin-top: 0;
+			}
+		}
+
+		.info p {
+			text-shadow: 1px 1px #000;
+		}
+
+		&.inline {
+			display: inline-flex;
+			flex-wrap: wrap;
+            gap: 1rem;
+
+			& * {
+				flex: 23%;
+			}
+		}
+	}
+</style>
