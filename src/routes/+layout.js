45f11b22d6fc03d6fa5c4fd5a1da33af66861763
Tagan H <tagnum.elite@gmail.com>
2024-04-25 21:48:39 +0200

    Squashed commit of the following:
    commit bd176b9c85e1a5f562a0883021ebea28041e9821
    Author: Tagan H <tagnum.elite@gmail.com>
    Date:   Thu Apr 25 21:45:36 2024 +0200
    
        Run prettier, pretty sure it's uglier now
    
        Also some quotes
    
    commit e9c414678693dd8814477139d0be630ce62b0fc7
    Author: Tagan H <tagnum.elite@gmail.com>
    Date:   Thu Apr 25 12:41:24 2024 +0200
    
        Create Quote as an component in lib
    
    commit d085d7f764017dd087389562d76656d46d14cdf0
    Author: Tagan H <tagnum.elite@gmail.com>
    Date:   Thu Apr 25 09:30:41 2024 +0200
    
        Shrink image
    
    commit e037c5d78a0e9126c5d69528a01d749ab0ddff86
    Author: Tagan H <tagnum.elite@gmail.com>
    Date:   Thu Apr 25 00:11:32 2024 +0200
    
        Some landing page work

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -11,9 +11,13 @@ export async function load() {
 			['Test', '/test']
 		],
 		socials: [
-			{slug: 'facebook', href: 'https://www.facebook.com/one.of.a.kind.original.hoyle', name: 'Facebook'},
-			{slug: 'instagram', href: 'https://instagram.com/ooako', name: 'Instagram'},
-			{slug: 'pinterest', href: 'https://za.pinterest.com/lindaannhoyle/', name: 'Pinterest'},
+			{
+				slug: 'facebook',
+				href: 'https://www.facebook.com/one.of.a.kind.original.hoyle',
+				name: 'Facebook'
+			},
+			{ slug: 'instagram', href: 'https://instagram.com/ooako', name: 'Instagram' },
+			{ slug: 'pinterest', href: 'https://za.pinterest.com/lindaannhoyle/', name: 'Pinterest' }
 		]
 	};
 }
