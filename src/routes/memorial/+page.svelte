b24cfd50451fe6d9f5d68edb4db555591da275d3
Tagan H <tagnum.elite@gmail.com>
2024-04-27 00:03:30 +0200

    Add memorial page and make quotes less WIP

---
+++ src/routes/memorial/+page.svelte
@@ -0,0 +1,28 @@
+<div class="video">
+    <div class="title">
+        <h1>Linda Hoyle's Memorial</h1>
+    </div>
+    <div class="container">
+<iframe width="1200" height="675" src="https://www.youtube-nocookie.com/embed/WTRzrZ46TR0?si=Bc0SkmgMBGiOl8xa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+</div>
+
+<style lang="scss">
+.video {
+    height: 100vh;
+    width: 100%;
+    text-align: center;
+
+    .title {
+        font-size: 2rem;
+    }
+
+    .container {
+        background-color: rgb(0, 0, 0);
+        padding: 5px;
+        border-radius: 5px;
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+}
+</style>
