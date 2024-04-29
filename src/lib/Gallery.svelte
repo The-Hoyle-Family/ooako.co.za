805e0e59a5c98dce9c9d08450ca84654d37eed80
Tagan H <tagnum.elite@gmail.com>
2024-04-29 19:23:23 +0200

    Implement a gallery library for the photos

---
+++ src/lib/Gallery.svelte
@@ -0,0 +1,47 @@
+<script>
+    /**
+	 * @type {{url: string, name: string, width?: Number}[]}
+	 */
+     export let pictures = [];
+
+     if (typeof pictures === 'string') {
+        // TODO: Look into dynamically importing a whole folder
+     }
+</script>
+
+<div class="gallery">
+    {#each pictures as pic}
+    <div class="image"><!-- style="flex-basis: {pic.width ? pic.width * 320 : 320}px" TODO: Width tag-->
+        <img src={pic.url} alt={pic.name}>
+    </div>
+    {:else}
+    <h1 class="warning">Gallery is empty</h1>
+    {/each}
+</div>
+<slot />
+
+<style lang="scss">
+.gallery {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 10px;
+
+    .image {
+        flex-basis: 320px;
+        transition: flex-basis 0.5s;
+
+        &:hover {
+            flex-basis: 350px;
+        }
+
+        img {
+            object-fit: cover;
+            max-width: 100%;
+            height: auto;
+            vertical-align: middle;
+            border-radius: 5px;
+        }
+    }
+}
+</style>
--- src/routes/memorial/+page.svelte
+++ src/routes/memorial/+page.svelte
@@ -1,8 +1,26 @@
 <script>
+    import Gallery from '$lib/Gallery.svelte';
     import YouTubeEmbed from '$lib/YoutubeEmbed.svelte'
+
+    let pics = [
+        {url: 'memorial/ashes.jpg', name: "Ashes"},
+        {url: 'memorial/entry-table.jpg', name: "Entry Table"},
+        {url: 'memorial/fireplace.jpg', name: "Fireplace"},
+        {url: 'memorial/looking-outside.jpg', name: "Looking Outside"},
+        {url: 'memorial/outside-brenton.jpg', name: "Looking Outside"},
+        {url: 'memorial/outside-ocena.jpg', name: "Looking Outside"},
+        {url: 'memorial/outside-pano.jpg', name: "Looking Pano", width: 2},
+        {url: 'memorial/poster.jpg', name: "Looking Pano"},
+        {url: 'memorial/stand-left-close.jpg', name: "Looking Pano"},
+        {url: 'memorial/stand-left.jpg', name: "Looking Pano"},
+        {url: 'memorial/stand-center.jpg', name: "Looking Pano"},
+        {url: 'memorial/stand-right-close.jpg', name: "Looking Pano"},
+        {url: 'memorial/stand-right.jpg', name: "Looking Pano"},
+        {url: 'memorial/stand.jpg', name: "Looking Pano"},
+    ];
 </script>
 
-<div class="video">
+<div class="container video">
 	<div class="title">
 		<h1>Linda Hoyle's Memorial</h1>
 	</div>
@@ -10,23 +28,30 @@
 		<YouTubeEmbed url="https://www.youtube-nocookie.com/embed/WTRzrZ46TR0?si=Bc0SkmgMBGiOl8xa"/>
 	</div>
 </div>
+<div class="container">
+    <Gallery pictures={pics} />
+</div>
 
 <style lang="scss">
-.video {
-    height: 100vh;
-    width: 100%;
-    text-align: center;
-
-    .title {
-        font-size: 2rem;
-    }
-
     .container {
-        background-color: rgb(0, 0, 0);
-        padding: 5px;
-        border-radius: 5px;
-        margin-left: 10px;
-        margin-right: 10px;
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+		text-align: center;
     }
-}
+	.video {
+		min-height: 100vh;
+		width: 100%;
+
+		.title {
+			font-size: 2rem;
+		}
+
+		.container {
+			background-color: rgb(0, 0, 0);
+			padding: 5px;
+			border-radius: 5px;
+			margin-left: 10px;
+			margin-right: 10px;
+		}
+	}
 </style>
