ce14eb2df7c99fdd5979436803a68bb7dbdc8709
Tagan H <tagnum.elite@gmail.com>
2024-05-02 10:04:39 +0200

    Implement a Photoswipe Gallery
    TODO: Alt, Image Width and Height

---
+++ src/lib/Photoswipe.svelte
@@ -0,0 +1,59 @@
+<script>
+    import PhotoSwipeLightbox from "photoswipe/lightbox";
+    import 'photoswipe/style.css';
+	import { onMount } from "svelte";
+
+    /**
+     * @type String
+     */
+    export let galleryID;
+     /**|{html: string}[]
+      * @type {{src: string, width: number, height: number, alt: string, srcset?: string}[]}
+      */
+     export let images;
+
+     onMount(() => {
+        let lightbox = new PhotoSwipeLightbox({
+            gallery: "#" + galleryID,
+            children: 'a',
+            pswpModule: () => import('photoswipe'),
+        });
+        lightbox.init();
+     });
+</script>
+
+<div class="pswp-gallery" id={galleryID}>
+    {#each images as image}
+    <a
+            href={image.src}
+            data-pswp-width={image.width}
+            data-pswp-height={image.height}
+            target="_blank"
+            rel="noreferrer"
+        >
+            <!--<img src={image.thumbnailURL} alt="" />-->
+            <img src={image.src} alt="">
+            </a>
+    {/each}
+</div>
+
+<style lang="scss">
+.pswp-gallery {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    
+    a {
+        flex-basis: 320px;
+        margin: 5px;
+
+        img {
+            object-fit: cover;
+            max-width: 100%;
+            height: auto;
+            vertical-align: middle;
+            border-radius: 5px;
+        }
+    }
+}
+</style>
--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,1 +1,21 @@
-<h1>Gallery</h1>
+<script lang="ts">
+	import Photoswipe from "$lib/Photoswipe.svelte";
+
+	const pictures : Record<string, string> = import.meta.glob(
+		'$lib/gallery/*.{avif,gif,heif,jpeg,jpg,png,tiff,webp}',
+		{
+			eager: true,
+			import: 'default'
+		}
+	);
+
+	const images = Object.keys(pictures).map((path) => {return {src: pictures[path], height: 100, width: 100, alt: ""}});
+</script>
+
+
+<div class="gallery">
+	<div class="title">
+		<h1>Gallery</h1>
+	</div>
+	<Photoswipe galleryID="artwork" images={images}/>
+</div>
