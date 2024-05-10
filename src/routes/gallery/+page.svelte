5e629ebe4166662bdad1868c7df6d3ed2dce51ab
Tagan H <tagnum.elite@gmail.com>
2024-05-10 15:40:56 +0200

    Use json file to list all gallery items

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,27 +1,13 @@
 <script lang="ts">
-	import Photoswipe from "$lib/Photoswipe.svelte";
-
-	const pictures : Record<string, string> = import.meta.glob(
-		'$lib/gallery/*.{avif,gif,heif,jpeg,jpg,png,tiff,webp}',
-		{
-			eager: true,
-			import: 'default'
-		}
-	);
-
-	const images = Object.keys(pictures).map((path) => {
-		let img = pictures[path];
-		let alt = img.split('\\').pop()?.split('/').pop();
-		return {src: img, height: 1000, width: 1000, alt: alt ? alt : "an image"}
-	});
+	import Photoswipe from '$lib/Photoswipe.svelte';
+	import galleryList from './gallery.json';
 </script>
-
 
 <div class="gallery">
 	<div class="title">
 		<h1>Gallery</h1>
 	</div>
-	<Photoswipe galleryID="artwork" images={images}/>
+	<Photoswipe galleryID="artwork" images={galleryList} />
 </div>
 
 <style lang="scss">
